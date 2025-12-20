bbe61a9bf71f352653386d5666cfbed52bf39a06
Guilherme Bessa <guibessa@alu.ufc.br>
2025-12-20 18:09:14 -0300

    atualizacao para ipv4

--- db/conn.go
+++ db/conn.go
@@ -3,19 +3,19 @@ package db
 import (
 	"fmt"
 
-	"github.com/guiezz/dashboard-api/config" // <--- Novo Import
+	"github.com/guiezz/dashboard-api/config"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-// Mude a assinatura para receber o *config.Config
 func ConnectDB(cfg *config.Config) (*gorm.DB, error) {
-	// Usa o método GetDSN() da configuração para obter a string de conexão
 	dsn := cfg.GetDSN()
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		PrepareStmt: false,
+	})
+
 	if err != nil {
-		// Mensagem de erro mais clara
 		return nil, fmt.Errorf("falha ao conectar no banco de dados (DSN: %s): %w", dsn, err)
 	}
 
